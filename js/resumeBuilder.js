109ab991698ae9babe321f1cbfd21c44a511c655
xblaise <ixblaise@gmail.com>
2015-04-13 15:17:53 -0700

    Replace Contact info
    Sorry I changed some css earlier(bored passing time) I’m pretty sure i
    changed it all back to the original.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,24 @@
 //$("#main").append(["blaise mcfarland"]);
 var name="Blaise";
 var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").append(formattedName);
+//$("#header").append(formattedName);
+var role="musician";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedName , formattedRole);
+
+
+var phone="714-777-7777";
+var mobile= "714-888-8888";
+var email= "ixblaise@gmail.com";
+var twitter = "imonethat";
+var gitHub="xblaise";
+var Alocation="Orange County";
+
+var formattedGeneric = HTMLcontactGeneric.replace("%contact%", "phone");
+var formattedPhone = formattedGeneric.replace("%data%", phone);
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+var formattedEmail = HTMLemail.replace("%data%", email);
+var formattedTwitter = HTMLtwitter.replace("%data%", twitter);
+var formattedGithub = HTMLgithub.replace("%data%", gitHub);
+var formattedLocation = HTMLlocation.replace("%data%", Alocation);
+$("#footerContacts").prepend(formattedPhone,formattedMobile,formattedEmail,formattedTwitter,formattedGithub,formattedLocation);
